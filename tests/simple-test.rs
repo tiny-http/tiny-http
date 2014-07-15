46c07bd6eb97187eba52997b731f96ac1384ad9c
Pierre Krieger <pierre.krieger1708@gmail.com>
2014-07-15 17:37:43 +0200

    Add first integration test

---
+++ tests/simple-test.rs
@@ -0,0 +1,21 @@
+extern crate httpd = "tiny-http";
+
+use std::io::net::tcp::TcpStream;
+
+#[test]
+fn basic_handling() {
+    let (server, port) = httpd::Server::new_with_random_port().unwrap();
+
+    let mut stream = std::io::net::tcp::TcpStream::connect("127.0.0.1", port);
+    write!(stream, "GET / HTTP/1.1\r\nHost: localhost\r\nConnection: close\r\n\r\n");
+
+    let request = server.recv().unwrap();
+    assert!(request.get_method().equiv(&"get"));
+    //assert!(request.get_url() == "/");
+    request.respond(httpd::Response::from_string(format!("hello world")));
+
+    server.try_recv().unwrap();
+
+    let content = stream.read_to_string().unwrap();
+    assert!(content.as_slice().ends_with("hello world"));
+}

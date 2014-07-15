06faeebecb0df1fe2f0e912828d917d72f550b30
Pierre Krieger <pierre.krieger1708@gmail.com>
2014-07-15 18:05:45 +0200

    Move unnecessary static functions

--- src/client.rs
+++ src/client.rs
@@ -40,90 +40,6 @@ impl ClientConnection {
         }
     }
 
-    /// Generates an IoError with invalid input.
-    /// This function is here because it is incredibly annoying to create this error.
-    fn gen_invalid_input(desc: &'static str) -> io::IoError {
-        io::IoError {
-            kind: io::InvalidInput,
-            desc: desc,
-            detail: None
-        }
-    }
-
-    /// Parses a "HTTP/1.1" string.
-    fn parse_http_version(version: &str) -> io::IoResult<HTTPVersion> {
-        let elems = version.splitn('/', 2).map(|e| e.to_string()).collect::<Vec<String>>();
-        if elems.len() != 2 {
-            return Err(ClientConnection::gen_invalid_input("Wrong HTTP version format"))
-        }
-
-        let elems = elems.get(1).as_slice().splitn('.', 2)
-            .map(|e| e.to_string()).collect::<Vec<String>>();
-        if elems.len() != 2 {
-            return Err(ClientConnection::gen_invalid_input("Wrong HTTP version format"))
-        }
-
-        match (from_str(elems.get(0).as_slice()), from_str(elems.get(1).as_slice())) {
-            (Some(major), Some(minor)) =>
-                Ok(HTTPVersion(major, minor)),
-            _ => Err(ClientConnection::gen_invalid_input("Wrong HTTP version format"))
-        }
-    }
-
-    /// Parses the request line of the request.
-    /// eg. GET / HTTP/1.1
-    fn parse_request_line(line: &str) -> io::IoResult<(Method, Path, HTTPVersion)> {
-        let mut words = line.words();
-
-        let method = words.next();
-        let path = words.next();
-        let version = words.next();
-
-        let (method, path, version) = match (method, path, version) {
-            (Some(m), Some(p), Some(v)) => (m, p, v),
-            _ => return Err(ClientConnection::gen_invalid_input("Missing element in request line"))
-        };
-
-        let method = match from_str(method) {
-            Some(method) => method,
-            None => return Err(ClientConnection::gen_invalid_input("Could not parse method"))
-        };
-
-        let path = match Path::parse(path) {
-            Ok(p) => p,
-            Err(_) => return Err(ClientConnection::gen_invalid_input("Wrong requested URL"))
-        };
-
-        let version = try!(ClientConnection::parse_http_version(version));
-
-        Ok((method, path, version))
-    }
-
-    /// Parses a header line.
-    /// eg. Host: example.com
-    fn parse_header(line: &str) -> io::IoResult<Header> {
-        let elems = line.splitn(':', 2).map(|e| e.to_string()).collect::<Vec<String>>();
-
-        if elems.len() <= 1 {
-            return Err(ClientConnection::gen_invalid_input(
-                "Wrong header format (no ':')"))
-        }
-        if elems.get(1).as_slice().chars().next() != Some(' ') {
-            return Err(ClientConnection::gen_invalid_input(
-                "Wrong header format (missing space after ':')"))
-        }
-
-        let field = match from_str(elems.get(0).as_slice()) {
-            None => return Err(ClientConnection::gen_invalid_input("Could not parse header")),
-            Some(f) => f
-        };
-
-        Ok(Header {
-            field: field,
-            value: elems.get(1).as_slice().slice_from(1).to_string()
-        })
-    }
-
     /// Reads a request from the stream.
     /// Blocks until the header has been read.
     fn read(&mut self) -> io::IoResult<Request> {
@@ -132,10 +48,10 @@ impl ClientConnection {
 
             // reading the request line
             let (method, path, version) =
-                try!(ClientConnection::parse_request_line(
+                try!(parse_request_line(
                     match lines.next() {
                         Some(line) => try!(line),
-                        None => return Err(ClientConnection::gen_invalid_input(
+                        None => return Err(gen_invalid_input(
                                     "Missing request line"))
                     }.as_slice().trim()
                 ));
@@ -149,7 +65,7 @@ impl ClientConnection {
                             let line = try!(line);
                             if line.as_slice().trim().len() == 0 { break };
                             headers.push(
-                                try!(ClientConnection::parse_header(line.as_slice().trim()))
+                                try!(parse_header(line.as_slice().trim()))
                             )
                         },
                         None => break
@@ -222,6 +138,90 @@ impl Iterator<Request> for ClientConnection {
     }
 }
 
+/// Generates an IoError with invalid input.
+/// This function is here because it is incredibly annoying to create this error.
+fn gen_invalid_input(desc: &'static str) -> io::IoError {
+    io::IoError {
+        kind: io::InvalidInput,
+        desc: desc,
+        detail: None
+    }
+}
+
+/// Parses a "HTTP/1.1" string.
+fn parse_http_version(version: &str) -> io::IoResult<HTTPVersion> {
+    let elems = version.splitn('/', 2).map(|e| e.to_string()).collect::<Vec<String>>();
+    if elems.len() != 2 {
+        return Err(gen_invalid_input("Wrong HTTP version format"))
+    }
+
+    let elems = elems.get(1).as_slice().splitn('.', 2)
+        .map(|e| e.to_string()).collect::<Vec<String>>();
+    if elems.len() != 2 {
+        return Err(gen_invalid_input("Wrong HTTP version format"))
+    }
+
+    match (from_str(elems.get(0).as_slice()), from_str(elems.get(1).as_slice())) {
+        (Some(major), Some(minor)) =>
+            Ok(HTTPVersion(major, minor)),
+        _ => Err(gen_invalid_input("Wrong HTTP version format"))
+    }
+}
+
+/// Parses the request line of the request.
+/// eg. GET / HTTP/1.1
+fn parse_request_line(line: &str) -> io::IoResult<(Method, Path, HTTPVersion)> {
+    let mut words = line.words();
+
+    let method = words.next();
+    let path = words.next();
+    let version = words.next();
+
+    let (method, path, version) = match (method, path, version) {
+        (Some(m), Some(p), Some(v)) => (m, p, v),
+        _ => return Err(gen_invalid_input("Missing element in request line"))
+    };
+
+    let method = match from_str(method) {
+        Some(method) => method,
+        None => return Err(gen_invalid_input("Could not parse method"))
+    };
+
+    let path = match Path::parse(path) {
+        Ok(p) => p,
+        Err(_) => return Err(gen_invalid_input("Wrong requested URL"))
+    };
+
+    let version = try!(parse_http_version(version));
+
+    Ok((method, path, version))
+}
+
+/// Parses a header line.
+/// eg. Host: example.com
+fn parse_header(line: &str) -> io::IoResult<Header> {
+    let elems = line.splitn(':', 2).map(|e| e.to_string()).collect::<Vec<String>>();
+
+    if elems.len() <= 1 {
+        return Err(gen_invalid_input(
+            "Wrong header format (no ':')"))
+    }
+    if elems.get(1).as_slice().chars().next() != Some(' ') {
+        return Err(gen_invalid_input(
+            "Wrong header format (missing space after ':')"))
+    }
+
+    let field = match from_str(elems.get(0).as_slice()) {
+        None => return Err(gen_invalid_input("Could not parse header")),
+        Some(f) => f
+    };
+
+    Ok(Header {
+        field: field,
+        value: elems.get(1).as_slice().slice_from(1).to_string()
+    })
+}
+
 #[cfg(test)]
 mod test {
     use super::ClientConnection;
@@ -229,23 +229,23 @@ mod test {
     #[test]
     fn test_parse_request_line() {
         let (method, path, ver) =
-            ClientConnection::parse_request_line("GET /hello HTTP/1.1").unwrap();
+            super::parse_request_line("GET /hello HTTP/1.1").unwrap();
 
         assert!(method.equiv(&"get"));
         assert!(path == from_str("/hello").unwrap());
         assert!(ver == ::common::HTTPVersion(1, 1));
 
-        assert!(ClientConnection::parse_request_line("GET /hello").is_err());
-        assert!(ClientConnection::parse_request_line("qsd qsd qsd").is_err());
+        assert!(super::parse_request_line("GET /hello").is_err());
+        assert!(super::parse_request_line("qsd qsd qsd").is_err());
     }
 
     #[test]
     fn test_parse_header() {
-        let header = ClientConnection::parse_header("Content-Type: text/html").unwrap();
+        let header = super::parse_header("Content-Type: text/html").unwrap();
 
         assert!(header.field.equiv(&"content-type"));
         assert!(header.value.as_slice() == "text/html");
 
-        assert!(ClientConnection::parse_header("hello world").is_err());
+        assert!(super::parse_header("hello world").is_err());
     }
 }

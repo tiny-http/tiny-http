46a8a3f783fbb67bc181c728e1bd3851789b85c3
Tomaka17 <pierre.krieger1708@gmail.com>
2014-07-18 09:26:09 +0200

    Replace simple-server by hello-world

---
+++ examples/hello-world.rs
@@ -0,0 +1,20 @@
+extern crate httpd = "tiny-http";
+
+use std::os;
+use std::sync::Arc;
+
+fn main() {
+    let server = Arc::new(httpd::Server::new_with_port(9975).unwrap());
+    println!("Now listening on port 9975");
+
+    for _ in range(0, os::num_cpus()) {
+        let server = server.clone();
+
+        spawn(proc() {
+            for rq in server.incoming_requests() {
+                let response = httpd::Response::from_string("hello world".to_string());
+                rq.respond(response);
+            }
+        })
+    }
+}

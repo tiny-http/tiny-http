f738f6e1dcfc4af8c043e12c78a61fc4c5ff6e5a
Corey Farwell <coreyf@rwell.org>
2015-05-04 09:17:57 -0400

    Remove unused files

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub use self::any::{AnyReader, AnyWriter};
 pub use self::closable_tcp_stream::ClosableTcpStream;
 pub use self::custom_stream::CustomStream;
 pub use self::encoding_decoder::EncodingDecoder;
@@ -23,9 +22,7 @@ pub use self::task_pool::TaskPool;
 
 use std::str::FromStr;
 
-mod any;
 mod closable_tcp_stream;
-mod custom_stream;
 mod encoding_decoder;
 mod equal_reader;
 mod sequential;

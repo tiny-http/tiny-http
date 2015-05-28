d94027d7c69533825af6e5ed098793eabfd3051f
Corey Farwell <coreyf@rwell.org>
2015-05-28 08:54:13 -0400

    Update readme-example.rs to match updated README.md example

--- examples/readme-example.rs
+++ examples/readme-example.rs
@@ -1,16 +1,18 @@
 extern crate tiny_http;
 
 fn main() {
-    let server = tiny_http::ServerBuilder::new().build().unwrap();
-
+    use tiny_http::{ServerBuilder, Response};
+    
+    let server = ServerBuilder::new().with_port(8000).build().unwrap();
+    
     for request in server.incoming_requests() {
         println!("received request! method: {:?}, url: {:?}, headers: {:?}",
             request.get_method(),
             request.get_url(),
             request.get_headers()
         );
-
-        let response = tiny_http::Response::from_string("hello world".to_string());
+    
+        let response = Response::from_string("hello world");
         request.respond(response);
     }
 }

8c7f30d515812afce3bb99a9610ca960e4bbb13f
Corey Farwell <coreyf@rwell.org>
2015-05-28 09:10:49 -0400

    Remove extraaneous whitespace

--- examples/readme-example.rs
+++ examples/readme-example.rs
@@ -2,16 +2,16 @@ extern crate tiny_http;
 
 fn main() {
     use tiny_http::{ServerBuilder, Response};
-    
+
     let server = ServerBuilder::new().with_port(8000).build().unwrap();
-    
+
     for request in server.incoming_requests() {
         println!("received request! method: {:?}, url: {:?}, headers: {:?}",
             request.get_method(),
             request.get_url(),
             request.get_headers()
         );
-    
+
         let response = Response::from_string("hello world");
         request.respond(response);
     }
